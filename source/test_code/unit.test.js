522ac148752684eb3c41ceb1940090a173c47cb0
Burniee <burnieburnsism@yahoo.com>
2021-11-19 22:54:41 -0800

    edited jsdoc.yml

--- source/test_code/unit.test.js
+++ source/test_code/unit.test.js
@@ -1,5 +1,9 @@
 /* eslint eol-last: ["error", "never"] */
 
+/**
+ * This function is a test file for jest and puppeteer
+ * @param {} no parameters yet
+ */
 test('test unit test', () =>{
   const x = 5;
   expect(x).toBe(5);
