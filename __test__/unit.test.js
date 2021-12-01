d0e5f2f162876d26c53330d36407932164339fda
CSE110-Dongyang-Huang <dohuang@ucsd.edu>
2021-11-30 23:09:41 -0800

    Add test files

--- __test__/unit.test.js
+++ __test__/unit.test.js
@@ -5,11 +5,11 @@
 const functions = require('../source/proof-of-concept/SpoonacularAPIWrapper.js');
 
 test('getIngredientQuery', () => {
-    expect(functions.getIngredientQuery([], {'ingredients': "apple", 'diet': "vegan", 'key': "123"}))
+    expect(functions.getIngredientQuery([], {'ingredients': ["apple"], 'diet': "vegan", 'key': "123"}))
     .toBe("?apiKey=123&includeIngredients=apple&diet=vegan");
 });
 
 test('processInitialResult', () => {
     expect(functions.processInitialResult([{"id": "123456"}, {"id": "654321"}]))
-    .toBe(["123456", "654321"]);
+    .toStrictEqual(["123456", "654321"]);
 });
