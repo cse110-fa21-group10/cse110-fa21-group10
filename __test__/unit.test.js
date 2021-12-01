5d3c65f7668ed437e8d0ff79c1bf6ec3d0c6f266
CSE110-Dongyang-Huang <dohuang@ucsd.edu>
2021-11-30 23:05:29 -0800

    Add test files

--- __test__/unit.test.js
+++ __test__/unit.test.js
@@ -1,5 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const functions = require('../source/proof-of-concept/SpoonacularAPIWrapper.js');
 
+test('getIngredientQuery', () => {
+    expect(functions.getIngredientQuery([], {'ingredients': "apple", 'diet': "vegan", 'key': "123"}))
+    .toBe("?apiKey=123&includeIngredients=apple&diet=vegan");
+});
+
 test('processInitialResult', () => {
-    expect(functions.processInitialResult([{"id": "123456"}, {"id": "654321"}])).toBe(["123456", "654321"]);
+    expect(functions.processInitialResult([{"id": "123456"}, {"id": "654321"}]))
+    .toBe(["123456", "654321"]);
 });
