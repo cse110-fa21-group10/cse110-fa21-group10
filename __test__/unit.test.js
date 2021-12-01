e67ae39c7bf237e0de1bea4ee1a9b12fc2f1f6c9
CSE110-Dongyang-Huang <dohuang@ucsd.edu>
2021-11-30 23:17:17 -0800

    Add test files

--- __test__/unit.test.js
+++ __test__/unit.test.js
@@ -4,12 +4,34 @@
 
 const functions = require('../source/proof-of-concept/SpoonacularAPIWrapper.js');
 
+// Testing getIngredientQuery
+test('getIngredientQuery', () => {
+    expect(functions.getIngredientQuery([], {'ingredients': [], 'diet': "", 'key': ""}))
+    .toBe("?apiKey=&includeIngredients=&diet=");
+});
+
 test('getIngredientQuery', () => {
     expect(functions.getIngredientQuery([], {'ingredients': ["apple"], 'diet': "vegan", 'key': "123"}))
     .toBe("?apiKey=123&includeIngredients=apple&diet=vegan");
 });
 
+test('getIngredientQuery', () => {
+    expect(functions.getIngredientQuery([], {'ingredients': ["apple", "egg", "oil"], 'diet': "vegetarian", 'key': "000"}))
+    .toBe("?apiKey=000&includeIngredients=apple,egg,oil&diet=vegetarian");
+});
+
+// Testing processInitialResult
+test('processInitialResult', () => {
+    expect(functions.processInitialResult([]))
+    .toStrictEqual([]);
+});
+
 test('processInitialResult', () => {
     expect(functions.processInitialResult([{"id": "123456"}, {"id": "654321"}]))
     .toStrictEqual(["123456", "654321"]);
+});
+
+test('processInitialResult', () => {
+    expect(functions.processInitialResult([{"id": "123456"}, {"id": "654321"}, {"id": "654123"}]))
+    .toStrictEqual(["123456", "654321", "654123"]);
 });
