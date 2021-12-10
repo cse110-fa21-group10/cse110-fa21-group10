4170c3cf426709aa85b26ee7543e47f48dbae770
Yixin Jiang <91864970+yij011@users.noreply.github.com>
2021-12-09 21:22:50 -0800

    Update FrontPage.js
    fix title create

--- source/FrontPage.js
+++ source/FrontPage.js
@@ -416,10 +416,10 @@ class RecipeCard extends HTMLElement {
   
       // ============== Container for Recipe Card Text ============== 
       // Here is the text part div
-      var text_div = document.createElement('recipe-title');
+      var text_div = document.createElement('div');
       text_div.classList.add('recipe-card-text');
       // get title image from recipe-card json
-      var title= document.createElement('recipe-title');
+      var title= document.createElement('div');
       title.classList.add('recipe-title');
       // get link image from recipe-card json
       var title_link = document.createElement('a');
