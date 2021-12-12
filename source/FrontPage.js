805505bd886085f501237363b6a99d2f54cab19c
raaghavravi <rravi@ucsd.edu>
2021-12-12 13:06:47 -0800

    Testing

--- source/FrontPage.js
+++ source/FrontPage.js
@@ -43,7 +43,7 @@ async function init() {
 
     // fetch the recipes and wait for them to load
     let fetchSuccessful = await loadRecommendations();
-    // if they didn't successfully load, quit the function
+    // if they dont successfully load, quit the function
     if (!fetchSuccessful) {
       console.log('Recipe fetch unsuccessful');
       return;
